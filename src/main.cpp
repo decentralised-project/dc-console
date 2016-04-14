da91f5e9e2aa87e1b7fe75e3a14ac1335d743c55
Decentralised <hello@decentralised-project.org>
2016-04-14 08:39:40 +0100

    added dc-core dependency.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <iomanip>
 #include <string>
 
+#include "core_client.hpp"
+
 #if WIN32
 	#include <windows.h>
 #else
@@ -12,53 +14,69 @@
 	#include <unistd.h>
 #endif
 
+using namespace dc::core;
+
+std::list<std::wstring> consoleLines;
+
+void updateConsole()
+{
+	int columns, rows;
+
+#if WIN32
+	std::system("cls");
+
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+#else
+	std::system("clear");
+
+	struct winsize w;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+	columns = w.ws_col;
+	rows = w.ws_row;
+#endif
+
+	for (int i = 0; i < rows - 1; ++i)
+	{
+		if (consoleLines.size() > rows - i - 2)
+		{
+			std::list<std::wstring>::iterator it = consoleLines.begin();
+			std::advance(it, rows - i - 2);
+
+			std::cout << std::setiosflags(std::ios::left) << std::setw(columns) << std::string(it->begin(), it->end());
+		}
+		else
+			std::cout << std::setiosflags(std::ios::left) << std::setw(columns) << " ";
+	}
+	std::cout << "> ";
+}
+
+void addToConsole(std::wstring from, std::wstring msg)
+{
+	consoleLines.push_front(std::wstring(L"[00:00] ").append(from).append(L": ").append(msg));
+	updateConsole();
+};
+
 int main(int argc, char** argv)
 {
-	std::list<std::wstring> consoleLines;
-	int columns, rows;
 	bool isRunning = true;
 
-	consoleLines.push_front(std::wstring(L"[00:00] System: dc-console v1.0"));
+	core_client core(&addToConsole);
+
+	addToConsole(L"dc-console", L"v1.0");
 
 	while (isRunning)
 	{
-#if WIN32
-		std::system("cls");
-
-		CONSOLE_SCREEN_BUFFER_INFO csbi;
-		GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-		columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-		rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-#else
-		std::system("clear");
-
-		struct winsize w;
-		ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-		columns = w.ws_col;
-		rows = w.ws_row;
-#endif
-
-		for (int i = 0; i < rows - 1; ++i)
-		{
-			if (consoleLines.size() > rows - i - 2)
-			{
-				std::list<std::wstring>::iterator it = consoleLines.begin();
-				std::advance(it, rows - i - 2);
-				
-				std::cout << std::setiosflags(std::ios::left) << std::setw(columns) << std::string(it->begin(), it->end());
-			}
-			else
-				std::cout << std::setiosflags(std::ios::left) << std::setw(columns) << " ";
-		}
-		std::cout << "> ";
+		updateConsole();
 
 		std::string line; 
 		std::getline(std::cin, line);
 		if (line == "exit")
 			isRunning = false;
 		else
-			consoleLines.push_front(std::wstring(L"[00:00] User: ").append(std::wstring(line.begin(), line.end())));
-
+			addToConsole(L"User", std::wstring(line.begin(), line.end()));
 	}
 
 	return 0;
